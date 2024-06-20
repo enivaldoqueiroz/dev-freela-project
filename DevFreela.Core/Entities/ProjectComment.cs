aed2911d6433de2ee94539ebb574cd79aa02b95e
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2024-06-20 12:14:39 -0300

    Implementação do projeto DevFreela.Core

---
+++ DevFreela.Core/Entities/ProjectComment.cs
@@ -0,0 +1,18 @@
+﻿namespace DevFreela.Core.Entities
+{
+    public class ProjectComment : BaseEntity
+    {
+        public ProjectComment(string content, int idProject, int idUser, DateTime createAt)
+        {
+            Content = content;
+            IdProject = idProject;
+            IdUser = idUser;
+            CreateAt = createAt;
+        }
+
+        public string Content { get; private set; }
+        public int IdProject { get; private set; }
+        public int IdUser { get; private set; }
+        public DateTime CreateAt { get; private set; }
+    }
+}
---
+++ DevFreela.Core/Entities/UserSkill.cs
@@ -0,0 +1,14 @@
+﻿namespace DevFreela.Core.Entities
+{
+    public class UserSkill : BaseEntity
+    {
+        public UserSkill(int idUser, string idSkill)
+        {
+            IdUser = idUser;
+            IdSkill = idSkill;
+        }
+
+        public int IdUser { get; private set; }
+        public string IdSkill { get; private set; }
+    }
+}
