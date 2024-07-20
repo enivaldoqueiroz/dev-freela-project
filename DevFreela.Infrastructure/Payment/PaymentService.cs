64201d3872d057b020e1f3250867ff9a6fa34c14
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2024-07-20 10:41:39 -0300

    Implementando o service de pagamento

---
+++ DevFreela.Infrastructure/Payment/PaymentService.cs
@@ -0,0 +1,15 @@
+﻿using DevFreela.Core.DTOs;
+using DevFreela.Core.Services;
+
+namespace DevFreela.Infrastructure.Payment
+{
+    public class PaymentService : IPaymentService
+    {
+        public Task<bool> ProcessPayment(PaymentInfoDto paymentInfoDto)
+        {
+            // Implementar lógica de pagamento com Gateway de Pagamento
+
+            return Task.FromResult(true);
+        }
+    }
+}
