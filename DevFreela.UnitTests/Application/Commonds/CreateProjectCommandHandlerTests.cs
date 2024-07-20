8b2f9abd9ee4c7944962ae20432ddfd7892db0a5
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2024-07-20 05:31:45 -0300

    Implementação teste unitario para o CreateProjectCommandHandler

---
+++ DevFreela.UnitTests/Application/Commonds/CreateProjectCommandHandlerTests.cs
@@ -0,0 +1,36 @@
+﻿using DevFreela.Application.Commands.CreateProject;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Moq;
+
+namespace DevFreela.UnitTests.Application.Commonds
+{
+    public class CreateProjectCommandHandlerTests
+    {
+        [Fact]
+        public async Task InputDataIsOk_Executed_ReturnProjectId()
+        {
+            // Arrange
+            var projectRepositoryMock = new Mock<IProjectRepository>();
+
+            var createProjectCommand = new CreateProjectCommand
+            {
+                Title = "Titulo de Teste",
+                Description = "Uma descrição aqui",
+                TotalCost = 50000,
+                IdClient = 1,
+                IdFreela = 2
+            };
+
+            var createProjectCommadHandler = new CreateProjectCommandHandler(projectRepositoryMock.Object);
+
+            // Act
+            var id = await createProjectCommadHandler.Handle(createProjectCommand, new CancellationToken());
+
+            // Assert
+            Assert.True(id >= 0);
+
+            projectRepositoryMock.Verify(pr => pr.AddAsync(It.IsAny<Project>()), Times.Once);
+        }
+    }
+}
